b0dec62afbd2112f53273d729ad715803b8646f0
Rui Pereira <ruijosemangas@gmail.com>
2015-07-01 11:58:44 +0100

    arrays done tests green

--- lib/enum/arrays.rb
+++ lib/enum/arrays.rb
@@ -1,24 +1,31 @@
 class Array
 
   def sum_of_numeric_elements
+    select { |element| element.is_a? Numeric }.inject(:+)
   end
 
   def product_of_numeric_elements
+    select { |element| element.is_a? Numeric }.inject(:*)
   end
 
   def even_numeric_elements
+    select { |element| element.is_a? Integer }.select { |number| number.even? }
   end
 
   def odd_numeric_elements
+    select { |element| element.is_a? Integer }.select { |number| number.odd? }
   end
 
   def string_elements
+    select { |element| element.is_a? String }
   end
 
   def lowercase_string_elements_as_uppercase
+    select { |el| el.is_a? String }.select { |word| ('a'..'z').include? word.chars.first }.map(&:capitalize)
   end
 
   def capitalized_elements
+    select { |el| el.is_a? String }.select { |word| ('A'..'Z').include? word.chars.first }
   end
 
 end
