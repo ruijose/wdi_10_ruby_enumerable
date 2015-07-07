0f4e5ca93426e1de2ce71c01db5913d07b1a2f76
Rui Pereira <ruijosemangas@gmail.com>
2015-07-07 16:27:41 +0100

    all done ; tests green

--- lib/enum/nested.rb
+++ lib/enum/nested.rb
@@ -18,21 +18,37 @@ class People
   end
 
   def ages_sum
+    @people.reduce(0) { |sum, person| sum += person.age }
   end
 
   def average_age
+    ages_sum / @people.count
   end
 
   def total_years_programming_experience_for_all_languages
+    @people.reduce(0) do |total, person|
+      person.years_language_experience.each do |key,value|
+        total += value
+      end
+      total
+    end
   end
 
   def favorite_food_frequency
+    @people.each_with_object(Hash.new(0)) do |person, result|
+      person.favorite_foods.each do |food|
+        result[food] += 1
+      end
+    end
   end
 
   def total_combined_years_language_experience(language)
-  end
-
-  def person_with_most_experience_in_language(language)
+    @people.reduce(0) do |total, person|
+      person.years_language_experience.each do |key,value|
+        total += value if key == language
+      end
+      total
+    end
   end
 
   private
